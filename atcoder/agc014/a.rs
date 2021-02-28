7d4cf12d4dc5cd461ade656f1b7ec6188ddb6c71
quietsato <quietsato@gmail.com>
2021-02-28 16:45:02 +0900

    Add agc014

---
+++ atcoder/agc014/a.rs
@@ -0,0 +1,67 @@
+fn main() {
+    let (a, b, c) = {
+        let mut buf = String::new();
+
+        std::io::stdin().read_line(&mut buf).unwrap();
+
+        let abc: Vec<u64> = buf
+            .trim_end()
+            .split_whitespace()
+            .map(|s| s.parse::<u64>().unwrap())
+            .collect();
+
+        let mut iter = abc.iter();
+
+        (
+            iter.next().unwrap().to_owned(),
+            iter.next().unwrap().to_owned(),
+            iter.next().unwrap().to_owned(),
+        )
+    };
+
+    match solve(a, b, c) {
+        Some(ans) => {
+            println!("{}", ans);
+        }
+        None => {
+            println!("-1");
+        }
+    }
+}
+
+fn solve(a: u64, b: u64, c: u64) -> Option<usize> {
+    if a == b && b == c {
+        if a.is_even() {
+            return None;
+        } else {
+            return Some(0);
+        }
+    }
+
+    let ans: usize = {
+        let mut i = 0usize;
+        let (mut a, mut b, mut c) = (a, b, c);
+        while a.is_even() && b.is_even() && c.is_even() {
+            let (a_, b_, c_) = (a / 2, b / 2, c / 2);
+            a = b_ + c_;
+            b = c_ + a_;
+            c = a_ + b_;
+
+            i += 1;
+        }
+        i
+    };
+
+    Some(ans)
+}
+
+trait IsEven {
+    fn is_even(&self) -> bool;
+}
+
+impl IsEven for u64 {
+    fn is_even(&self) -> bool {
+        self & 1 == 0
+    }
+}
+
